ec9a44316f6dd9dd42fd35dda6a0463f21d41dca
Mohamed Hakkim <hakkim.mkp@gmail.com>
2025-10-28 23:25:50 +0530

    Update stick-to-me.js

--- view/frontend/web/js/stick-to-me.js
+++ view/frontend/web/js/stick-to-me.js
@@ -1,11 +1,3 @@
-/*!
- * jQuery Stick to Me plugin
- *
- * @author: Guilherme Assemany
- * @version: 1.0
- * @requires jQuery 1.6.1 or later
- *
- */
 (function ($) {
 
     $.stickToMe = function ( configs ) {
