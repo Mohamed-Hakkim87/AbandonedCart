a3a9e75ef33ce530fefbc7cdda35bd4f6274cbfd
Mohamed Hakkim <hakkim.mkp@gmail.com>
2025-10-28 23:32:22 +0530

    Update Campaign.php

--- Model/Campaign.php
+++ Model/Campaign.php
@@ -1,15 +1,4 @@
 <?php
-/**
- * Class Campaign
- *
- * PHP version 7
- *
- * @category Risecommerce
- * @package  Risecommerce_AbandonedCart
- * @author   Risecommerce <magento@risecommerce.com>
- * @license  https://www.risecommerce.com  Open Software License (OSL 3.0)
- * @link     https://www.risecommerce.com
- */
 namespace Hikmadh\AbandonedCart\Model;
 
 
